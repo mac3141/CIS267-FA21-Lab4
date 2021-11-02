dedf10998de047583fcdfc429c6f09f62c6af414
Madison Cagle <78162094+mac3141@users.noreply.github.com>
2021-11-01 21:43:09 -0500

    Update app
    Add notes, copy styles and functions from Lab 3, change names to match style sheets

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
+const pokemon_count = 20;
+
 const app = Vue.createApp({
     data() {
         return {
-            allPokemon: [
-                { id: 1, name: "bulbasaur", type: ["grass", "poison"] },
-                { id: 2, name: "ivysaur", type: ["grass", "poison"] }
-            ],
-            partyPokemon: [{ id: 2, name: "ivysaur", type: "grass" }],
+            allPokemon: [],
+            partyPokemon: [],
             filteredPokemon: [],
             maxPartySize: 6
         }
     },
     methods: {
-        async loadAllPokemon() {
+        // async loadAllPokemon() {
+        //     // load all Pokemon from API and save to allPokemon
+        //     await fetchAllPokemon();
+        //     renderPokemon(allPokemon);
+        // },
+        async fetchAllPokemon() {
             // load all Pokemon from API and save to allPokemon
+            for (let i = 1; i <= pokemon_count; i++) {
+                let pokemon = await this.getPokemonData(i);
+                // let species = await getPokemonSpecies(i);
+                pokemon.isFavorite = false;
+                // pokemon.isLegendary = species.is_legendary;
+                this.allPokemon.push(pokemon);
+            }
         },
-        async getPokemon(id) {
+        async getPokemonData(id) {
             // get an individual pokemon
+            const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+            const response = await fetch(url);
+            const data = await response.json();
+
+            return data;
         },
-        async renderPokemon(pokemonArray) {
-            // render an array of pokemon objects as cards
-        },
+        // async renderPokemon(pokemonArray) {
+        //     // render an array of pokemon objects as cards
+        //     pokemonArray.forEach(pokemon => createPokemonCard(pokemon));
+        // },
         filterPokemon() {
             // set filteredPokemon to matching pokemon based on search query
         },
@@ -39,3 +56,5 @@ const app = Vue.createApp({
         }
     }
 }).mount("#app");
+
+app.fetchAllPokemon();
